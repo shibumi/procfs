7aaa21713ae0e45ef853fed862633620b5f058dc
Christian Rebischke <Chris.Rebischke@posteo.de>
2018-06-27 13:47:48 +0200

    This commit provides support for CIFS
    Support for CIFS on GNU/Linux with SMB1 and SMB2

---
+++ cifs/parse_cifs.go
@@ -0,0 +1,217 @@
+// Copyright 2018 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cifs
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// ParseClientStats returns stats read from /proc/fs/cifs/Stats
+func ParseClientStats(r io.Reader) (*ClientStats, error) {
+	stats := &ClientStats{}
+	stats.Header = make(map[string]uint64)
+	scanner := bufio.NewScanner(r)
+	// Parse header
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, regexpHeader := range regexpHeaders {
+			match := regexpHeader.FindStringSubmatch(line)
+			if 0 == len(match) {
+				continue
+			}
+			for index, name := range regexpHeader.SubexpNames() {
+				if 0 == index || "" == name {
+					continue
+				}
+				value, err := strconv.ParseUint(match[index], 10, 64)
+				if nil != err {
+					continue
+				}
+				stats.Header[name] = value
+			}
+			break
+		}
+		if strings.HasPrefix(line, "Total vfs") {
+			break
+		}
+	}
+	// Parse Shares
+	var tmpSMB1Stats *SMB1Stats
+	var tmpSMB2Stats *SMB2Stats
+	var tmpSessionIDs *SessionIDs
+	legacy := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if legacy {
+			for _, regexpSMB1 := range regexpSMB1s {
+				match := regexpSMB1.FindStringSubmatch(line)
+				if 0 == len(match) {
+					if strings.HasPrefix(line, "SMBs:") && !(strings.Contains(line, "breaks")) {
+						legacy = false
+						tmpSMB2Stats = &SMB2Stats{
+							Stats: make(map[string]map[string]uint64),
+						}
+						stats.SMB2Stats = append(stats.SMB2Stats, tmpSMB2Stats)
+						re := regexp.MustCompile("[0-9]+")
+						find_smb := re.FindAllString(line, 1)
+						tmpSMB2Stats.Stats["smbs"] = make(map[string]uint64)
+						value, err := strconv.ParseUint(find_smb[0], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSMB2Stats.Stats["smbs"]["smbs"] = value
+						break
+					}
+					continue
+				}
+				for index, name := range regexpSMB1.SubexpNames() {
+					if 0 == index || "" == name {
+						continue
+					}
+					switch name {
+					case "sessionID":
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSessionIDs = &SessionIDs{
+							SessionID: value,
+						}
+					case "server":
+						if "" != match[index] {
+							tmpSessionIDs.Server = match[index]
+						}
+					case "share":
+						if "" != match[index] {
+							tmpSessionIDs.Share = match[index]
+						}
+					case "smbs":
+						tmpSMB1Stats = &SMB1Stats{
+							Stats: make(map[string]uint64),
+						}
+						stats.SMB1Stats = append(stats.SMB1Stats, tmpSMB1Stats)
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSMB1Stats.Stats[name] = value
+					default:
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							continue
+						}
+						if 0 == tmpSMB1Stats.SessionIDs.SessionID {
+							tmpSMB1Stats.SessionIDs.SessionID = tmpSessionIDs.SessionID
+							tmpSMB1Stats.SessionIDs.Server = tmpSessionIDs.Server
+							tmpSMB1Stats.SessionIDs.Share = tmpSessionIDs.Share
+
+						}
+						tmpSMB1Stats.Stats[name] = value
+					}
+				}
+				break
+			}
+		} else {
+			var keyword string
+			for _, regexpSMB2 := range regexpSMB2s {
+				match := regexpSMB2.FindStringSubmatch(line)
+				if 0 == len(match) {
+					if strings.HasPrefix(line, "SMBs:") && strings.Contains(line, "breaks") {
+						legacy = true
+						tmpSMB1Stats = &SMB1Stats{
+							Stats: make(map[string]uint64),
+						}
+						stats.SMB1Stats = append(stats.SMB1Stats, tmpSMB1Stats)
+						re := regexp.MustCompile("[0-9]+")
+						find_smb := re.FindAllString(line, 2)
+						smbs, err := strconv.ParseUint(find_smb[0], 10, 64)
+						if nil != err {
+							continue
+						}
+						breaks, err := strconv.ParseUint(find_smb[1], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSMB1Stats.Stats["smbs"] = smbs
+						tmpSMB1Stats.Stats["breaks"] = breaks
+
+						break
+					}
+					continue
+				}
+				for index, name := range regexpSMB2.SubexpNames() {
+					if 0 == index || "" == name {
+						continue
+					}
+					switch name {
+					case "sessionID":
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSessionIDs = &SessionIDs{
+							SessionID: value,
+						}
+					case "server":
+						if "" != match[index] {
+							tmpSessionIDs.Server = match[index]
+						}
+					case "share":
+						if "" != match[index] {
+							tmpSessionIDs.Share = match[index]
+						}
+					case "smbs":
+						tmpSMB2Stats = &SMB2Stats{
+							Stats: make(map[string]map[string]uint64),
+						}
+						stats.SMB2Stats = append(stats.SMB2Stats, tmpSMB2Stats)
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							continue
+						}
+						tmpSMB2Stats.Stats[name] = make(map[string]uint64)
+						tmpSMB2Stats.Stats[name][name] = value
+
+					default:
+						value, err := strconv.ParseUint(match[index], 10, 64)
+						if nil != err {
+							keyword = match[index]
+							tmpSMB2Stats.Stats[keyword] = make(map[string]uint64)
+							continue
+						}
+						if 0 == tmpSMB2Stats.SessionIDs.SessionID {
+							tmpSMB2Stats.SessionIDs.SessionID = tmpSessionIDs.SessionID
+							tmpSMB2Stats.SessionIDs.Server = tmpSessionIDs.Server
+							tmpSMB2Stats.SessionIDs.Share = tmpSessionIDs.Share
+
+						}
+						tmpSMB2Stats.Stats[keyword][name] = value
+					}
+				}
+				break
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error scanning SMB file: %s", err)
+	}
+
+	return stats, nil
+}
---
+++ cifs/parse_cifs_test.go
@@ -0,0 +1,123 @@
+// Copyright 2018 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cifs_test
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/prometheus/procfs/cifs"
+)
+
+func TestNewCifsRPCStats(t *testing.T) {
+	tests := []struct {
+		name    string
+		content string
+		stats   *cifs.ClientStats
+		invalid bool
+	}{
+		{
+			name:    "invalid file",
+			content: "invalid",
+			invalid: true,
+		}, {
+			name: "SMB1 statistics",
+			content: `Resources in use
+CIFS Session: 1
+Share (unique mount targets): 2
+SMB Request/Response Buffer: 1 Pool size: 5
+SMB Small Req/Resp Buffer: 1 Pool size: 30
+Operations (MIDs): 0
+
+0 session 0 share reconnects
+Total vfs operations: 16 maximum at one time: 2
+
+1) \\server\share
+SMBs: 9 Oplocks breaks: 0
+Reads:  0 Bytes: 0
+Writes: 0 Bytes: 0
+Flushes: 0
+Locks: 0 HardLinks: 0 Symlinks: 0
+Opens: 0 Closes: 0 Deletes: 0
+Posix Opens: 0 Posix Mkdirs: 0
+Mkdirs: 0 Rmdirs: 0
+Renames: 0 T2 Renames 0
+FindFirst: 1 FNext 0 FClose 0`,
+			stats: &cifs.ClientStats{
+				Header: map[string]uint64{
+					"operations":         0,
+					"sessionCount":       0,
+					"sessions":           1,
+					"shareReconnects":    0,
+					"shares":             2,
+					"smbBuffer":          1,
+					"smbPoolSize":        5,
+					"smbSmallBuffer":     1,
+					"smbSmallPoolSize":   30,
+					"totalMaxOperations": 2,
+					"totalOperations":    16,
+				},
+				SMB1Stats: []*cifs.SMB1Stats{
+					&cifs.SMB1Stats{
+						SessionIDs: cifs.SessionIDs{
+							SessionID: 1,
+							Server:    "server",
+							Share:     "\\share",
+						},
+						Stats: map[string]uint64{
+							"breaks":      0,
+							"closes":      0,
+							"deletes":     0,
+							"fClose":      0,
+							"fNext":       0,
+							"findFirst":   1,
+							"flushes":     0,
+							"hardlinks":   0,
+							"locks":       0,
+							"mkdirs":      0,
+							"opens":       0,
+							"posixMkdirs": 0,
+							"posixOpens":  0,
+							"reads":       0,
+							"readsBytes":  0,
+							"renames":     0,
+							"rmdirs":      0,
+							"smbs":        9,
+							"symlinks":    0,
+							"t2Renames":   0,
+							"writes":      0,
+							"writesBytes": 0,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stats, err := cifs.ParseClientStats(strings.NewReader(tt.content))
+
+			if tt.invalid && nil == err {
+				t.Fatal("expected an error, but none occured")
+			}
+			if !tt.invalid && nil != err {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if want, have := tt.stats, stats; !reflect.DeepEqual(want, have) {
+				t.Fatalf("unexpected CIFS Stats:\nwant:\n%v\nhave:\n%v", want, have)
+			}
+		})
+	}
+}
